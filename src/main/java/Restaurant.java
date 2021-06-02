5b627842db5b3382b6ecbe6ee4a924eae3b18c3c
syedan <syedanees85@gmail.com>
2021-06-02 06:42:03 +0530

    Part 2: Solution
    * Implement getMenu() for Restaurant
    
    * Implement Restaurant.isRestaurantOpen along with unit tests
    
    * Implement RestaurantService.findRestaurantByName along with unit tests
    
    * DRY common code in specs

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -1,3 +1,4 @@
+
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -18,15 +19,17 @@ public class Restaurant {
     }
 
     public boolean isRestaurantOpen() {
-        return true;
-        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+        LocalTime currentTime = getCurrentTime();
+        if (currentTime.isAfter(this.openingTime) && currentTime.isBefore(this.closingTime)) {
+            return true;
+        }
+        return false;
     }
 
     public LocalTime getCurrentTime(){ return  LocalTime.now(); }
 
     public List<Item> getMenu() {
-        return null;
-        //DELETE ABOVE RETURN STATEMENT AND WRITE CODE HERE
+        return this.menu;
     }
 
     private Item findItemByName(String itemName){
--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -1,36 +1,61 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@ExtendWith(MockitoExtension.class)
 class RestaurantTest {
     Restaurant restaurant;
-    //REFACTOR ALL THE REPEATED LINES OF CODE
+
+    @BeforeEach
+    public void beforeEachTest() {
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant = new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+    }
 
     //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-    //-------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN INTO ANY TROUBLE
     @Test
     public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
-        //WRITE UNIT TEST CASE HERE
+        //Arrange
+        LocalTime currentTime = LocalTime.parse("12:00:00");
+        Restaurant spyRestaurant = Mockito.spy(restaurant);
+        Mockito.doReturn(currentTime).when(spyRestaurant).getCurrentTime();
+
+        //Act
+        Boolean isOpen = spyRestaurant.isRestaurantOpen();
+
+        //Assert
+        assertEquals(true, isOpen);
+        Mockito.verify(spyRestaurant).getCurrentTime();
     }
 
     @Test
     public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
-        //WRITE UNIT TEST CASE HERE
+        //Arrange
+        LocalTime currentTime = LocalTime.parse("23:30:00");
+        Restaurant spyRestaurant = Mockito.spy(restaurant);
+        Mockito.doReturn(currentTime).when(spyRestaurant).getCurrentTime();
 
+        //Act
+        Boolean isOpen = spyRestaurant.isRestaurantOpen();
+
+        //Assert
+        assertEquals(false, isOpen);
+        Mockito.verify(spyRestaurant).getCurrentTime();
     }
-
     //<<<<<<<<<<<<<<<<<<<<<<<<<OPEN/CLOSED>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
     //>>>>>>>>>>>>>>>>>>>>>>>>>>>MENU<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     @Test
     public void adding_item_to_menu_should_increase_menu_size_by_1(){
-        LocalTime openingTime = LocalTime.parse("10:30:00");
-        LocalTime closingTime = LocalTime.parse("22:00:00");
-        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
         restaurant.addToMenu("Sweet corn soup",119);
         restaurant.addToMenu("Vegetable lasagne", 269);
 
@@ -40,9 +65,6 @@ class RestaurantTest {
     }
     @Test
     public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
-        LocalTime openingTime = LocalTime.parse("10:30:00");
-        LocalTime closingTime = LocalTime.parse("22:00:00");
-        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
         restaurant.addToMenu("Sweet corn soup",119);
         restaurant.addToMenu("Vegetable lasagne", 269);
 
@@ -52,9 +74,6 @@ class RestaurantTest {
     }
     @Test
     public void removing_item_that_does_not_exist_should_throw_exception() {
-        LocalTime openingTime = LocalTime.parse("10:30:00");
-        LocalTime closingTime = LocalTime.parse("22:00:00");
-        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
         restaurant.addToMenu("Sweet corn soup",119);
         restaurant.addToMenu("Vegetable lasagne", 269);
 
